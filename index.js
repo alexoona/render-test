1b074811c1120afc92ae9cb68e7d6f59c234d624
안효섭 <74307443+alexoona@users.noreply.github.com>
2026-05-29 18:15:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,25 @@ const app = express();
 
 app.get("/", async (req, res) => {
   try {
-    const r = await fetch("https://blinkit.com");
+    const r = await fetch(
+      "https://blinkit.com/prn/x/prid/716896",
+      {
+        headers: {
+          "User-Agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/137.0.0.0 Safari/537.36",
+          "Accept":
+            "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+          "Accept-Language": "en-US,en;q=0.9"
+        }
+      }
+    );
 
     const html = await r.text();
 
     res.json({
       status: r.status,
-      contentType: r.headers.get("content-type"),
-      hasBlinkit: html.includes("Blinkit"),
+      title: html.match(/<title>(.*?)<\/title>/i)?.[1] || "NO TITLE",
+      length: html.length,
       preview: html.slice(0, 2000)
     });
 
@@ -20,10 +31,4 @@ app.get("/", async (req, res) => {
       error: e.message
     });
   }
-});
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log("Running on", PORT);
 });
